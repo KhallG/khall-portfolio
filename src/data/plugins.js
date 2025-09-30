e407ed6623c38e1b01d37b6387c5962f0777bb6a
Kholl <119129333+KhallG@users.noreply.github.com>
2025-09-30 18:55:19 +0200

    fix image src

--- src/data/plugins.js
+++ src/data/plugins.js
@@ -2,33 +2,33 @@ const plugins = [
   {
     title: "Combine Fabricator",
     description: "A Half-Life: Alyx inspired machine that allows Combine units to create weapons, ammunition, and equipment.",
-    image: "../assets/image/inventory.png",
+    image: "/khall-portfolio/assets/image/inventory.png",
     video: "https://www.youtube.com/watch?v=ZH9Naj9w8uA"
   },
   {
     title: "Infestation Control",
     description: "A Half-Life: Alyx inspired system that allows Workforce units to identify, contain, and eliminate Xen infestations.",
-    image: "../assets/image/ui.png"
+    image: "/khall-portfolio/assets/image/ui.png"
   },
   {
     title: "Advanced Crafting System",
     description: "Flexible crafting plugin with recipes, levels, and resource management.",
-    image: "../assets/image/crafting.png"
+    image: "/khall-portfolio/assets/image/crafting.png"
   },
   {
     title: "Better Items",
     description: "Revamped item system with new functionalities, rarity tiers, weight and stacks.",
-    image: "../assets/image/crafting.png"
+    image: "/khall-portfolio/assets/image/crafting.png"
   },
   {
     title: "Bodybags",
     description: "A simple system for managing and disposing of bodies, featuring interactions and corpse incineration.",
-    image: "../assets/image/crafting.png"
+    image: "/khall-portfolio/assets/image/crafting.png"
   },
   {
     title: "Zombie",
     description: "A system where headcrab NPCs can take control of players, turning them into playable zombies with unique mechanics.",
-    image: "../assets/image/ui.png"
+    image: "/khall-portfolio/assets/image/ui.png"
   }
 ];
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,33 +2,33 @@ const projects = [
   {
     title: "Project: Résonance HL:A - 2025",
     description: "Development & systems inspired by Half-Life: Alyx, adapted for immersive Garry's Mod roleplay.",
-    image: "../assets/image/hla.png"
+    image: "/khall-portfolio/assets/image/hla.png"
   }, 
   {
     title: "Résonance Network - 2025",
     description: "Founder & lead developer of the Resonance Network, creating advanced and immersive servers for Garry's Mod.",
-    image: "../assets/image/resonance_network.png",
+    image: "/khall-portfolio/assets/image/resonance_network.png",
     link: "https://github.com/RESONANCE-NETWORK"
   },
   {
     title: "Project: Résonance SCP - 2025",
     description: "A complete SCP-oriented framework for Helix, featuring containment mechanics, security systems, and events.",
-    image: "../assets/image/scp.png"
+    image: "/khall-portfolio/assets/image/scp.png"
   },
   {
     title: "Untitled Apocalypse RP - 2024",
     description: "A complete Apocalypse-oriented framework for Helix, featuring survival mechanics, class systems, and events.",
-    image: "/assets/img/apocalypse.jpg"
+    image: "/khall-portfolio/assets/image/apocalypse.jpg"
   },
   {
     title: "Trex SCPRP - 2022/2023",
     description: "An old SCP RP-oriented roleplay DarkRP server.",
-    image: "/assets/img/apocalypse.jpg"
+    image: "/khall-portfolio/assets/image/apocalypse.jpg"
   },
   {
     title: "Abyss SW:TOR RP - 2020",
     description: "An old Star Wars: The Old Republic-oriented roleplay DarkRP server.",
-    image: "/assets/img/apocalypse.jpg"
+    image: "/khall-portfolio/assets/image/apocalypse.jpg"
   }
 ];
 
