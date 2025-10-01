4b9e5f0b0515b46b88a82ccddcc8bb263656da63
Kholl <119129333+KhallG@users.noreply.github.com>
2025-10-01 03:04:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     outDir: "dist",
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
+        entryFileNames: `assets/[hash].js`,
+        chunkFileNames: `assets/[hash].js`,
+        assetFileNames: `assets/[hash][extname]`
       }
     }
   }
