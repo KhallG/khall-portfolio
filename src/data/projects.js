30646be40b7702b11acda74b0e8046db8d039be4
Kholl <119129333+KhallG@users.noreply.github.com>
2025-10-02 16:39:25 +0200

    added some infos about my projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -25,8 +25,9 @@ const projects = [
     icon: "assets/image/scp.png",
     media: [
       { type: 'video', src: 'https://cdn.discordapp.com/attachments/1403471295865159842/1416799958304161984/Desktop_2025.09.14_-_16.53.16.01_online-video-cutter.com.mp4?ex=68de92c8&is=68dd4148&hm=9f08a87bf0c68381efadf9c160cb0d6ed08d178dfcb440472ffdf6f7a4be46f7&' },
-      { type: 'image', src: 'https://media.discordapp.net/attachments/567765409252638913/1416422532009033748/image.png?ex=68de84c7&is=68dd3347&hm=e865e7b1349fb360d718cb37cb4e29c6d3d0e02d332af912ad4c17be2396736d&=&format=webp&quality=lossless&width=1522&height=856' },
-      { type: 'image', src: 'https://media.discordapp.net/attachments/567765409252638913/1416423497827356822/image.png?ex=68de85ad&is=68dd342d&hm=d07021dbf5e57a045a54711d3159cb13af54f28eaf7eae56eea4531770931003&=&format=webp&quality=lossless&width=1522&height=856' },
+      { type: 'image', src: 'https://media.discordapp.net/attachments/1403471295865159842/1416422273648431355/image.png?ex=68dfd609&is=68de8489&hm=e01b4d0dfea38cc58e2de52caccf3bc3a3a3065f80af5f24d7401e5da3ba242b&=&format=webp&quality=lossless&width=1522&height=856' },
+      { type: 'image', src: 'https://media.discordapp.net/attachments/567765409252638913/1416423677444231218/image.png?ex=68dfd758&is=68de85d8&hm=b057d03fb61685d053e668053d546659b0da881551e7724b8497024dd0ad832f&=&format=webp&quality=lossless&width=1522&height=856' },
+      { type: 'image', src: '      https://media.discordapp.net/attachments/567765409252638913/1416423958869446717/image.png?ex=68dfd79b&is=68de861b&hm=46a16287936b889554aed5e7ef1f1b6ad993ddad3e66f58f46c15f462350e422&=&format=webp&quality=lossless' },
     ],
     links: [
       { url: "https://discord.gg/9cTrYNh7mt", label: "linkLabel1" },
@@ -36,12 +37,11 @@ const projects = [
   {
     key: "apocalypse",
     media: [
-      { type: 'video', src: 'https://cdn.discordapp.com/attachments/567765409252638913/1286863722563764305/Desktop_2024.09.21_-_03.34.44.04.mp4?ex=68de7ffc&is=68dd2e7c&hm=14a8bdd339c4e3b7dd9c5345f98a420fea5d04ca1b999b8b96b6105c5104dc95&' },
+      { type: 'video', src: 'https://cdn.discordapp.com/attachments/567765409252638913/1286863722563764305/Desktop_2024.09.21_-_03.34.44.04.mp4?ex=68dfd17c&is=68de7ffc&hm=dccd3b218ef853ad60e74722cdb7d593ff99d964ffd4158bdaf7fd89af173a8f&' },
       { type: 'video', src: 'https://media.discordapp.net/attachments/1267668804511731752/1293298641666052197/Desktop_2024.10.08_-_21.46.44.04.mp4?ex=68ded6b8&is=68dd8538&hm=b80d087523685a420f792b8365c2d20c4057c51209d732abf2370cd716010fe7&' },
       { type: 'video', src: 'https://cdn.discordapp.com/attachments/567765409252638913/1290343468769476702/Desktop_2024.09.30_-_18.00.21.01.mp4?ex=68dea280&is=68dd5100&hm=6e264f630c75f90c567c6e2574323e3a21a924401bd89d7b56d79c72eecdf1bf&' },
       { type: 'video', src: 'https://cdn.discordapp.com/attachments/567765409252638913/1286052482803433584/Desktop_2024.09.18_-_21.20.22.01.mp4?ex=68ded835&is=68dd86b5&hm=713763bf6b00d7a6721e459c580b5968a5b79cf4e2c39d8ae9ee76ee6ae367e9&' },
       { type: 'video', src: 'https://cdn.discordapp.com/attachments/567765409252638913/1286052486716854272/Desktop_2024.09.18_-_21.23.20.04.mp4?ex=68ded836&is=68dd86b6&hm=908621991f8bd08347847cd8da1472c41197437198099beedcb256da04a7e988&' },
-      { type: 'video', src: 'https://cdn.discordapp.com/attachments/567765409252638913/1202978601356628088/2024-02-02_15-03-01.mp4?ex=68de8720&is=68dd35a0&hm=118eb7477e5dd1d5778bd4f3fe520b533fdf99cb61d314665e64834fac76a070& ' },
       { type: 'image', src: 'https://media.discordapp.net/attachments/567765409252638913/1268991890879942658/image.png?ex=68debdcb&is=68dd6c4b&hm=bf4ced8590f090a98cd659778b7983f75581ba8ef2e8f27c501ca8f8f7698c65&=&format=webp&quality=lossless' },
       { type: 'image', src: 'https://media.discordapp.net/attachments/567765409252638913/1268991891639111730/image.png?ex=68debdcb&is=68dd6c4b&hm=cfb2e1c78e4f84f5742143fe6df073ac69c40e33660e1ae50900aad62c3fe564&=&format=webp&quality=lossless' },
     ],
